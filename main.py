8c687d6b92b3a89428a9edb06ca4dee6db8aa5fb
irung25 <encangwahyudin025@gmail.com>
2025-12-28 22:11:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import numpy as np
+from ta.momentum import RSIIndicator
 
 def get_trades(pair):
     url = f"https://indodax.com/api/trades/{pair}"
@@ -25,21 +26,28 @@ def build_candles(trades, timeframe="4h"):
 def calculate_ema(df, period):
     return df["close"].ewm(span=period, adjust=False).mean()
 
+def calculate_rsi(df, period=14):
+    rsi = RSIIndicator(close=df["close"], window=period)
+    return rsi.rsi()
+
 if __name__ == "__main__":
     trades = get_trades("btcidr")
     df = build_candles(trades)
 
     df["ema50"] = calculate_ema(df, 50)
     df["ema200"] = calculate_ema(df, 200)
+    df["rsi"] = calculate_rsi(df)
 
     last = df.iloc[-1]
 
     print("EMA50 :", last["ema50"])
     print("EMA200:", last["ema200"])
+    print("RSI14 :", last["rsi"])
 
-    if last["ema50"] > last["ema200"]:
-        print("TREND: UPTREND")
-    elif last["ema50"] < last["ema200"]:
-        print("TREND: DOWNTREND")
+    trend_ok = last["ema50"] > last["ema200"]
+    rsi_ok = 55 <= last["rsi"] <= 68
+
+    if trend_ok and rsi_ok:
+        print("MOMENTUM: VALID (BOLEH CARI ENTRY)")
     else:
-        print("TREND: SIDEWAYS")
+        print("NO TRADE: Momentum / Trend belum valid")
