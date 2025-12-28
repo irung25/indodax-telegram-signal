2996b1cea5407ebfd46dce1cb88cfad71fbf614b
irung25 <encangwahyudin025@gmail.com>
2025-12-28 21:42:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Bot Indodax Signal AKTIF & SIAP")
+import requests
+
+def get_ticker(pair):
+    url = f"https://indodax.com/api/ticker/{pair}"
+    response = requests.get(url)
+    data = response.json()
+    return data["ticker"]["last"]
+
+if __name__ == "__main__":
+    price = get_ticker("btcidr")
+    print(f"Harga BTC/IDR saat ini: {price}")
