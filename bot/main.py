12e66a50207bc810ed77d054085f6ab8a3e7efad
irung25 <encangwahyudin025@gmail.com>
2025-12-28 20:17:11 +0700

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,16 @@
-print("Bot siap")
+import os
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_test_message():
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": "✅ Bot Indodax Signal AKTIF & SIAP!"
+    }
+    requests.post(url, data=payload)
+
+if __name__ == "__main__":
+    send_test_message()
